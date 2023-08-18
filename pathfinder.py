fa83d882fbcb0a1659230be1e806f37113253487
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2023-08-18 13:57:22 -0500

    More optimizations

--- pathfinder.py
+++ pathfinder.py
@@ -11,7 +11,6 @@ import matplotlib.colors as mcolors
 from matplotlib.pyplot import figure
 from scipy.special import comb
 from scipy import interpolate
-from rdp import rdp
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
@@ -26,7 +25,7 @@ def generate_field(year, robot_radius):
 
   Returns
   -------
-  2D array representing FRC field with obstacles
+  array : 2D array representing FRC field with obstacles
   """
 
   global frc_field
@@ -99,10 +98,10 @@ def get_neighbors(array, node):
   x, y = node
   for i in (-1, 0, 1):
     for j in (-1, 0, 1):
-      if i != 0 or j != 0:
-        neighbor = (x + i, y + j)
-        if 0 <= neighbor[0] < array.shape[0] and 0 <= neighbor[1] < array.shape[1] and array[neighbor[0]][neighbor[1]] == 0:
-          neighbors.append(neighbor)
+      if i == 0 and j == 0: continue
+      neighbor = (x + i, y + j)
+      if 0 <= neighbor[0] < array.shape[0] and 0 <= neighbor[1] < array.shape[1] and array[neighbor[0]][neighbor[1]] == 0:
+        neighbors.append(neighbor)
   return neighbors
 
   # If the goal is not reachable, return False
@@ -122,10 +121,10 @@ def is_turn(current, neighbor, previous):
   bool : True if the neighbor is a turn, False otherwise
   """
 
-  current_direction = tuple(x-y for x,y in zip(previous, current))
-  new_direction = tuple(x-y for x,y in zip(current, neighbor))
+  current_direction = tuple(x - y for x, y in zip(previous, current))
+  new_direction = tuple(x - y for x, y in zip(current, neighbor))
 
-  return current_direction == new_direction
+  return current_direction != new_direction
 
 def astar(array, start, goal):
   """A* algorithm for finding the shortest path between two points in a graph.
@@ -162,11 +161,11 @@ def astar(array, start, goal):
 
     # If the current node is the goal, return the path
     if current == goal:
-      data = []
+      path = []
       while current in came_from:
-        data.append(current)
+        path.append(current)
         current = came_from[current]
-      return data
+      return path[::-1]
 
     # Mark the current node as closed
     close_set.add(current)
@@ -202,7 +201,7 @@ def astar_with_turns(array, start, goal):
 
   # Initialize the data structures
   close_set = set()
-  came_from = {}
+  came_from = {start: start}
   g_score = {start: 0}
   turn_score = {start: 0}
   turn_penalty = 5
@@ -219,11 +218,12 @@ def astar_with_turns(array, start, goal):
 
     # If the current node is the goal, return the path
     if current == goal:
-      data = []
+      path = []
       while current in came_from:
-        data.append(current)
+        path.append(current)
+        if current == start: break
         current = came_from[current]
-      return data
+      return path[::-1]
 
     # Mark the current node as closed
     close_set.add(current)
@@ -232,20 +232,40 @@ def astar_with_turns(array, start, goal):
     for neighbor in get_neighbors(array, current):
       # If the neighbor is not closed and the tentative g_score is less than the current g_score
       if neighbor not in close_set and g_score[current] + array[neighbor[0]][neighbor[1]] < g_score.get(neighbor, float('inf')):
+        # Add the neighbor to the came_from map
+        came_from[neighbor] = current
         # Update the g_score and turn_score of the neighbor
         g_score[neighbor] = g_score[current] + array[neighbor[0]][neighbor[1]]
         turn_score[neighbor] = turn_score[current] + 1
         f_score[neighbor] = g_score[neighbor] + turn_score[neighbor] + heuristic(neighbor, goal)
-        if current is not start:
-          if is_turn(current, neighbor, came_from[current]):
-            f_score[neighbor] += turn_penalty
+        if is_turn(current, neighbor, came_from[current]):
+          f_score[neighbor] -= turn_penalty
         # Add the neighbor to the priority queue
         heapq.heappush(oheap, (f_score[neighbor], neighbor))
-        # Add the neighbor to the came_from map
-        came_from[neighbor] = current
 
   # If the goal is not reachable, return False
   return False
+
+def smooth_path(path):
+  """Smooth path
+
+  Parameters
+  ----------
+  path : Array of points representing path
+
+  Returns
+  -------
+  array : Array of points representing smoothed path
+  """
+
+  # Extract x and y coordinates from path
+  coords = list(zip(*path))
+
+  # Smooth the path using spline interpolation
+  tck, *rest = interpolate.splprep([coords[0], coords[1]])
+  x_smooth, y_smooth = interpolate.splev(np.linspace(0, 1, 80), tck)
+
+  return list(zip(x_smooth, y_smooth))
 
 def plot_path(path, start, goal):
   """Plot path graphically
@@ -255,28 +275,16 @@ def plot_path(path, start, goal):
   path : Array of tuples representing path
   """
   
-  # Extract x and y coordinates from route list
-  path = rdp(path, epsilon=0.5) # generate a simplified path
- 
-  x_coords = []
-  y_coords = []
-  for i in (range(0,len(path))):
-    x = path[i][0]
-    y = path[i][1]
-    x_coords.append(x)
-    y_coords.append(y)
-
-  # smooth the path using a spline
-  tck, *rest = interpolate.splprep([x_coords, y_coords])
-  x_smooth, y_smooth = interpolate.splev(np.linspace(0, 1, 100), tck)
+  # Extract x and y coordinates from path
+  coords = list(zip(*path))
 
   # plot map and path
   fig, ax = plt.subplots()
   cmap = mcolors.ListedColormap(['green', 'grey', 'lightgrey'])
   ax.imshow(frc_field, cmap=cmap)
   ax.scatter(start[1], start[0], marker="*", color="yellow", s=200)
-  ax.scatter(goal[1], goal[0], marker="*", color="red", s=200)
-  ax.plot(y_smooth, x_smooth, color="blue")
+  ax.scatter(goal[1], goal[0], marker="*", color="purple", s=200)
+  ax.plot(coords[1], coords[0], color="blue")
   plt.show()
 
 
@@ -285,12 +293,14 @@ def main():
   start = m_to_cm(11.00, 0.50)
   goal = m_to_cm(15.10, 6.75)
 
+  # Generate FRC field
   generate_field(2023, 0.35)
 
+  # Calculate and smooth path
   path = astar_with_turns(frc_field, start, goal)
-  path = path + [start]
-  path = path[::-1]
+  path = smooth_path(path)
 
+  # Visualize path
   plot_path(path, start, goal)
 
 if __name__ == "__main__":
