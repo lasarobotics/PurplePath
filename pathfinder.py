079f0bc00670485712b86eb57bb58de1763ae83a
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2023-08-20 02:23:03 -0500

    Such fast, very optimize, wow

--- pathfinder.py
+++ pathfinder.py
@@ -1,20 +1,8 @@
 #!/bin/python
 
-import os
-import random
 import numpy as np
 import heapq
-import json
-import math
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-from matplotlib.pyplot import figure
-from scipy.special import comb
 from scipy import interpolate
-from shapely.geometry import Point
-from shapely.geometry.polygon import Polygon
-
-frc_field = np.full((1650, 810), 0)
 
 def generate_field(year, robot_radius):
   """Generate field for year
@@ -22,18 +10,31 @@ def generate_field(year, robot_radius):
   Parameters
   ----------
   year : Field year to load
+  robot_radius: Radius of robot in meters
 
   Returns
   -------
   array : 2D array representing FRC field with obstacles
   """
-
-  global frc_field
-  if os.path.isfile(str(year) + '.npy'):
-    frc_field = np.load(str(year) + '.npy', allow_pickle=True)
-    return
   
+  import os
+  
+  frc_field = np.full((1650, 810), 0)
+  
+  field_cache_file = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'fields',
+    str(year) + '.npy'
+  )
+  if os.path.isfile(field_cache_file):
+    frc_field = np.load(field_cache_file, allow_pickle=True)
+    return frc_field
+  
+  field_json_file = os.path.join(os.path.realpath(__file__), 'fields', str(year) + '.json')
   with open('fields/' + str(year) + '.json') as file:
+    import json
+    from shapely.geometry import Point
+    from shapely.geometry.polygon import Polygon
     obstacles = json.load(file)['obstacles']
     print(obstacles)
     
@@ -50,8 +51,9 @@ def generate_field(year, robot_radius):
           print(str(point) + " is within buffer range of " + obstacle['name'] + "!")
           frc_field[idx] = 2
           break
-    np.save(str(year), frc_field, allow_pickle=True)
-        
+    np.save(field_cache_file, frc_field, allow_pickle=True)
+    return frc_field
+
 def m_to_cm(x, y):
   """Convert xy coordinate in meters to centimeters
   
@@ -66,8 +68,22 @@ def m_to_cm(x, y):
   """
   return int(x * 100), int(y * 100)
 
-def heuristic(a, b):
-  """Heuristic function for path scoring 
+def cm_to_m(x, y):
+  """Convert xy coordinate in centimeters to meters
+  
+  Parameters
+  ----------
+  x : x value
+  y : y value
+
+  Returns
+  -------
+  tuple representing point with cm units
+  """
+  return float(x / 100), float(y / 100)
+
+def distance(a, b):
+  """Calculates the Pythagorean distance between two points
   
   Parameters
   ----------
@@ -81,12 +97,31 @@ def heuristic(a, b):
   """
   return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
 
-def get_neighbors(array, node):
-  """Returns the neighbors of a given node in the graph.
+def manhattan_distance(a, b):
+  """Calculates the Manhattan distance between two points
+  
+  Parameters
+  ----------
+  a : tuple representing point
+  b : tuple representing point
+
+  Returns
+  -------
+  int or float
+    Estimated distance to goal
+  """
+  
+  x_diff = a[0] - b[0]
+  y_diff = a[1] - b[1]
+
+  return abs(x_diff) + abs(y_diff)
+
+def get_neighbors(field, node, goal):
+  """Returns the neighbors of a given node in the field.
 
   Parameters
   ----------
-  array : 2D array representing the graph
+  field : 2D array representing the field
   node : Tuple representing the node
 
   Returns
@@ -94,13 +129,14 @@ def get_neighbors(array, node):
   array : List of tuples representing the neighbors of the node
   """
 
+  increment = 1 if manhattan_distance(node, goal) < 10 else 10
   neighbors = []
   x, y = node
-  for i in (-1, 0, 1):
-    for j in (-1, 0, 1):
+  for i in (-increment, 0, +increment):
+    for j in (-increment, 0, +increment):
       if i == 0 and j == 0: continue
       neighbor = (x + i, y + j)
-      if 0 <= neighbor[0] < array.shape[0] and 0 <= neighbor[1] < array.shape[1] and array[neighbor[0]][neighbor[1]] == 0:
+      if 0 <= neighbor[0] < field.shape[0] and 0 <= neighbor[1] < field.shape[1] and field[neighbor[0]][neighbor[1]] == 0:
         neighbors.append(neighbor)
   return neighbors
 
@@ -121,76 +157,17 @@ def is_turn(current, neighbor, previous):
   bool : True if the neighbor is a turn, False otherwise
   """
 
-  current_direction = tuple(x - y for x, y in zip(previous, current))
-  new_direction = tuple(x - y for x, y in zip(current, neighbor))
+  current_direction = (previous[0] - current[0], previous[1] - current[1])
+  new_direction = (current[0] - neighbor[0], current[1] - neighbor[1])
 
   return current_direction != new_direction
 
-def astar(array, start, goal):
-  """A* algorithm for finding the shortest path between two points in a graph.
-
-  Parameters
-  ----------
-  array : 2D array representing the field
-  start : Tuple representing the start node
-  goal : Tuple representing the goal node
-
-  Returns
-  -------
-  array : Array of tuples representing the shortest path from start to goal
-  """
-
-  # List of positions that have already been considered
-  close_set = set()
-
-  # Dictionary containing all routes we've taken
-  came_from = {}
-
-  # Scores
-  g_score = {start: 0}
-  f_score = {start: heuristic(start, goal)}
-
-  # Create a priority queue to store the nodes to be explored
-  oheap = []
-  heapq.heappush(oheap, (f_score[start], start))
-
-  # While there are nodes to be explored
-  while oheap:
-    # Get the node with the lowest f_score
-    current = heapq.heappop(oheap)[1]
-
-    # If the current node is the goal, return the path
-    if current == goal:
-      path = []
-      while current in came_from:
-        path.append(current)
-        current = came_from[current]
-      return path[::-1]
-
-    # Mark the current node as closed
-    close_set.add(current)
-
-    # For each neighbor of the current node
-    for neighbor in get_neighbors(array, current):
-      # If the neighbor is not closed and the tentative g_score is less than the current g_score
-      if neighbor not in close_set and g_score[current] + array[neighbor[0]][neighbor[1]] < g_score.get(neighbor, float('inf')):
-        # Update the g_score and f_score of the neighbor
-        g_score[neighbor] = g_score[current] + array[neighbor[0]][neighbor[1]]
-        f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goal)
-        # Add the neighbor to the priority queue
-        heapq.heappush(oheap, (f_score[neighbor], neighbor))
-        # Add the neighbor to the came_from map
-        came_from[neighbor] = current
-
-  # If the goal is not reachable, return False
-  return False
-
-def astar_with_turns(array, start, goal):
+def astar(field, start, goal):
   """A* algorithm for finding the shortest path between two points in a graph, with the fewest number of turns.
 
   Parameters
   ----------
-  array : 2D array representing the field
+  field : 2D array representing the field
   start : Tuple representing the start node
   goal : Tuple representing the goal node
 
@@ -199,13 +176,15 @@ def astar_with_turns(array, start, goal):
   array : Array of tuples representing the shortest path from start to goal
   """
 
-  # Initialize the data structures
+  ## List of positions that have already been considered
   close_set = set()
-  came_from = {start: start}
-  g_score = {start: 0}
-  turn_score = {start: 0}
-  turn_penalty = 5
-  f_score = {start: g_score[start] + heuristic(start, goal)}
+  
+  # Dictionary containing all routes we've taken
+  came_from = {}
+  
+  # Scores
+  g_score = { start: 0 }
+  f_score = { start: g_score[start] + manhattan_distance(start, goal) }
 
   # Create a priority queue to store the nodes to be explored
   oheap = []
@@ -221,7 +200,6 @@ def astar_with_turns(array, start, goal):
       path = []
       while current in came_from:
         path.append(current)
-        if current == start: break
         current = came_from[current]
       return path[::-1]
 
@@ -229,17 +207,14 @@ def astar_with_turns(array, start, goal):
     close_set.add(current)
 
     # For each neighbor of the current node
-    for neighbor in get_neighbors(array, current):
-      # If the neighbor is not closed and the tentative g_score is less than the current g_score
-      if neighbor not in close_set and g_score[current] + array[neighbor[0]][neighbor[1]] < g_score.get(neighbor, float('inf')):
+    for neighbor in get_neighbors(field, current, goal):
+      # If the neighbor is not closed and the current f_score is greater than the neighbor f_score
+      if neighbor not in close_set and f_score[current] > f_score.get(neighbor, 0):
         # Add the neighbor to the came_from map
         came_from[neighbor] = current
-        # Update the g_score and turn_score of the neighbor
-        g_score[neighbor] = g_score[current] + array[neighbor[0]][neighbor[1]]
-        turn_score[neighbor] = turn_score[current] + 1
-        f_score[neighbor] = g_score[neighbor] + turn_score[neighbor] + heuristic(neighbor, goal)
-        if is_turn(current, neighbor, came_from[current]):
-          f_score[neighbor] -= turn_penalty
+        # Update the g_score and f_score of the neighbor
+        g_score[neighbor] = g_score[current] + manhattan_distance(neighbor, start)
+        f_score[neighbor] = g_score[neighbor] + manhattan_distance(neighbor, goal)
         # Add the neighbor to the priority queue
         heapq.heappush(oheap, (f_score[neighbor], neighbor))
 
@@ -263,25 +238,28 @@ def smooth_path(path):
 
   # Smooth the path using spline interpolation
   tck, *rest = interpolate.splprep([coords[0], coords[1]])
-  x_smooth, y_smooth = interpolate.splev(np.linspace(0, 1, 80), tck)
+  x_smooth, y_smooth = interpolate.splev(np.linspace(0, 1, 25), tck)
 
   return list(zip(x_smooth, y_smooth))
 
-def plot_path(path, start, goal):
+def plot_path(field, path, start, goal):
   """Plot path graphically
 
   Parameters
   ----------
+  field : 2D array representing field
   path : Array of tuples representing path
   """
-  
+  import matplotlib.pyplot as plt
+  import matplotlib.colors as mcolors
+
   # Extract x and y coordinates from path
   coords = list(zip(*path))
-
+  
   # plot map and path
   fig, ax = plt.subplots()
   cmap = mcolors.ListedColormap(['green', 'grey', 'lightgrey'])
-  ax.imshow(frc_field, cmap=cmap)
+  ax.imshow(field, cmap=cmap)
   ax.scatter(start[1], start[0], marker="*", color="yellow", s=200)
   ax.scatter(goal[1], goal[0], marker="*", color="purple", s=200)
   ax.plot(coords[1], coords[0], color="blue")
@@ -289,19 +267,21 @@ def plot_path(path, start, goal):
 
 
 def main():
-  # start point and goal
-  start = m_to_cm(11.00, 0.50)
-  goal = m_to_cm(15.10, 6.75)
-
   # Generate FRC field
-  generate_field(2023, 0.35)
+  field = generate_field(2023, 0.35)
+  
+  # start point and goal
+  start = m_to_cm(14.00, 0.50)
+  goal = m_to_cm(15.10, 6.75)
+  # start = m_to_cm(8.00, 5.00)
+  # goal = m_to_cm(14.50, 3.50)
 
   # Calculate and smooth path
-  path = astar_with_turns(frc_field, start, goal)
+  path = astar(field, start, goal)
   path = smooth_path(path)
 
   # Visualize path
-  plot_path(path, start, goal)
+  #plot_path(field, path, start, goal)
 
 if __name__ == "__main__":
   main()
